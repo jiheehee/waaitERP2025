28c25837597ca0fdff3cbf407d7c621e3649814c
solmi <solmi@DESKTOP-RE7PAE5>
2025-09-07 22:42:52 +0900

    <sm> test1file 수정

--- waaitfinal/src/main/java/com/waait/common/TestClassFile.java
+++ waaitfinal/src/main/java/com/waait/common/TestClassFile.java
@@ -5,5 +5,15 @@ public class TestClassFile {
 	//Test입니다.
 	//Test후 삭제 예정
 	//System.out.println("테스트");
-
+	
+	private String name;
+	private int age;
+	
+	public TestClassFile() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void testMethod() {
+		System.out.println(name + age);
+	}
 }
